126fd6f3e909423b0415346a1f7cb81602c238b6
Mohd-Huzaifa-Hussain <121728623+Mohd-Huzaifa-Hussain@users.noreply.github.com>
2024-09-22 07:14:28 +0530

    Update SpringBootBackendApplication.java

--- src/main/java/in/ineuron/nitin/SpringBootBackendApplication.java
+++ src/main/java/in/ineuron/nitin/SpringBootBackendApplication.java
@@ -2,6 +2,9 @@ package in.ineuron.nitin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootBackendApplication {
@@ -9,5 +12,15 @@ public class SpringBootBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendApplication.class, args);
 	}
+
+	@Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("https://employeeapp-production.up.railway.app");
+         }
+      };
+   }
 
 }
